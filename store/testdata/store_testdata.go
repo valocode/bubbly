74351651427212a12ae3c3560a32eb0afd8e44d8
Oliver Frolovs <ofrolovs@verifa.io>
2021-03-26 07:47:57 +0200

    Make Store test schema and data loaders context-aware.

--- store/testdata/store_testdata.go
+++ store/testdata/store_testdata.go
@@ -11,30 +11,34 @@ import (
 	"github.com/zclconf/go-cty/cty"
 )
 
-func Tables(t *testing.T, fromFile string) core.Tables {
+func Tables(t *testing.T, bCtx *env.BubblyContext, fromFile string) core.Tables {
 	t.Helper()
 
-	bCtx := env.NewBubblyContext()
 	tableWrapper := struct {
 		Tables core.Tables `hcl:"table,block"`
 	}{}
+
 	body, err := parser.MergedHCLBodies(bCtx, fromFile)
 	require.NoErrorf(t, err, "failed to parse tables")
+
 	err = parser.DecodeExpandBody(bCtx, body, &tableWrapper, cty.NilVal)
 	require.NoErrorf(t, err, "failed to decode tables")
+
 	return tableWrapper.Tables
 }
 
-func DataBlocks(t *testing.T, fromFile string) core.DataBlocks {
+func DataBlocks(t *testing.T, bCtx *env.BubblyContext, fromFile string) core.DataBlocks {
 	t.Helper()
 
-	bCtx := env.NewBubblyContext()
 	dataWrapper := struct {
 		Data core.DataBlocks `hcl:"data,block"`
 	}{}
+
 	body, err := parser.MergedHCLBodies(bCtx, fromFile)
 	require.NoErrorf(t, err, "failed to parse data blocks")
+
 	err = parser.DecodeExpandBody(bCtx, body, &dataWrapper, cty.NilVal)
 	require.NoErrorf(t, err, "failed to decode data blocks")
+
 	return dataWrapper.Data
 }
